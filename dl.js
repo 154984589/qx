daf3c6da136446a2dcf1669b17cae95b2a11d2af
154984589 <67886568+154984589@users.noreply.github.com>
2024-07-07 16:46:19 +0800

    Update dl.js

--- dl.js
+++ dl.js
@@ -19,5 +19,13 @@ console.log(body)
 console.log(json1)
 json1.data[0].status = 1
 json1.data[1].status = 1
+json1.data[2].status = 1
+json1.data[3].status = 1
+json1.data[4].status = 1
+json1.data[5].status = 1
+json1.data[6].status = 1
+json1.data[7].status = 1
+json1.data[8].status = 1
+json1.data[9].status = 1
 body = JSON.stringify(json1)
 $done({body});
